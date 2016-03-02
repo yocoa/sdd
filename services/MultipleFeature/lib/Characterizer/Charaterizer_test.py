279f5eb77c710286cfd690388812edea091f3b93
zhangxiaoyang <zhangxiaoyang.hit@gmail.com>
2016-03-02 13:05:09 +0800

    modified Charaterizer_test

--- services/MultipleFeature/lib/Characterizer/Charaterizer_test.py
+++ services/MultipleFeature/lib/Characterizer/Charaterizer_test.py
@@ -2,9 +2,9 @@
 # encoding: utf-8
 
 import unittest
-import  Charaterizer as C
+import Charaterizer as C
 
-class TestNetworkFeature(unittest.TestCase):
+class TestCharaterizer(unittest.TestCase):
     
     def test_convert(self):
         self.assertTrue(C.convert('google.com'))
