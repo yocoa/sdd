0608b0e79687994913ab3f8236952e7984f88011
zhangxiaoyang <zhangxiaoyang.hit@gmail.com>
2016-04-01 16:24:34 +0800

    updated

--- ext/test1/main.py
+++ ext/test1/main.py
@@ -3,13 +3,15 @@
 
 import sys
 import re
+import random
 
 from lib import DataBox
 import subprocess
 
 def load_train(path):
     train_list = []
-    filenames = ['lexical.bad', 'lexical.good', 'type.bad', 'type.good']
+    #filenames = ['lexical.bad', 'lexical.good', 'type.bad', 'type.good']
+    filenames = ['type.bad', 'type.good']
     for name in filenames:
         with open('/'.join([path, name]), 'r') as f:
             for line in f:
@@ -32,6 +34,12 @@ def load_data(path):
             data_list.append(domain)
     return data_list
 
+def chunks(l, n):
+     chunk_size = len(l) / n
+     for i in xrange(0, n):
+             yield l[i*chunk_size:i*chunk_size+chunk_size]
+
+
 if __name__ == '__main__':
     selection, number = 'default', '30'
     if len(sys.argv) > 2:
@@ -39,6 +47,16 @@ if __name__ == '__main__':
 
     if selection == 'default':
         arff_filename = DataBox.run(load_train('train/'), load_data('data/')[:int(number)])
+    if selection == 'random':
+        copied_data = [i for i in load_data('data/')]
+        random.shuffle(copied_data)
+        arff_filename = DataBox.run(load_train('train/'), copied_data[:int(number)])
+    if selection == 'random2':
+        copied_data = [i for i in load_data('data/')]
+        copied_data2 = []
+        for chunk in list(chunks(copied_data, int(number))):
+            copied_data2.append(random.choice(chunk))
+        arff_filename = DataBox.run(load_train('train/'), copied_data2[:int(number)])
     cmd = 'java -classpath lib/weka.jar weka.classifiers.trees.RandomForest -t %s -i' % arff_filename
     output = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True).communicate()[0]
     tmp = re.search(r'Weighted Avg\.(.*\..*)', output).group(1).strip()
--- ext/test1/wrapper.sh
+++ ext/test1/wrapper.sh
@@ -1,27 +1,63 @@
 #!/usr/bin/env bash
 
-number=10
-rm -f result.txt
+function default_method0()
+{
+    rm -f default_result0.txt
+    python main.py default 0 >> default_result0.txt
+}
 
-while true
-do
-    if (($number > 100))
-    then
-        break
-    fi
-    echo default, $number
-    python main.py default $number >> result.txt
-    ((number+=10))
-done
+function default_method()
+{
+    number=30
+    rm -f default_result.txt
 
-number=100
-while true
-do
-    if (($number > 2000))
-    then
-        break
-    fi
-    echo default, $number
-    python main.py default $number >> result.txt
-    ((number+=100))
-done
+    while true
+    do
+        if (($number > 600))
+        then
+            break
+        fi
+        echo default, $number
+        python main.py default $number >> default_result.txt
+        ((number+=30))
+    done
+}
+
+function random_method()
+{
+    number=30
+    rm -f random_result.txt
+
+    while true
+    do
+        if (($number > 600))
+        then
+            break
+        fi
+        echo random, $number
+        python main.py random $number >> random_result.txt
+        ((number+=30))
+    done
+}
+
+function random_method2()
+{
+    number=30
+    rm -f random_result2.txt
+
+    while true
+    do
+        if (($number > 600))
+        then
+            break
+        fi
+        echo random2, $number
+        python main.py random2 $number >> random_result2.txt
+        ((number+=30))
+    done
+}
+
+default_method0
+#default_method
+#random_method
+#random_method2
